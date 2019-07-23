7eab3b4d6614b8aa4311e7ca3138aa9e0b2968fc
Warren Loy Harper II <warrenharper2@att.net>
2019-07-23 17:38:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+read_from_hash = [[:monkeys, 1],animals]
 end
 
 def update_counting_hash(hash, key)
