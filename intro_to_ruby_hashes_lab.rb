2d9dbd7ac82ff7c59c3f62b1102b7441120d3671
Warren Loy Harper II <warrenharper2@att.net>
2019-07-23 17:51:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,7 @@ read_from_hash = { "dogs=>{:"animals"}}
 end
 
 def update_counting_hash(hash, key)
+person = {
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
